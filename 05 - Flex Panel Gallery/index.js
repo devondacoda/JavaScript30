3fe30cd78e092b7286844fa5fd8f2fe5055396a2
Devon JohnBaptiste <devondevon@outlook.com>
2017-10-23 16:35:31 -0400

    day 5 complete

---
+++ 05 - Flex Panel Gallery/index.js
@@ -0,0 +1,11 @@
+const panels = document.querySelectorAll('.panel');
+function toggleOpen() {
+  this.classList.toggle('open');
+}
+function toggleActive(evt) {
+  if (evt.propertyName.includes('flex')) this.classList.toggle('open-active');
+}
+panels.forEach((panel) => {
+  panel.addEventListener('click', toggleOpen);
+  panel.addEventListener('transitionend', toggleActive);
+});
